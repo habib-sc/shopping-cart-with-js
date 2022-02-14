b394d7ff0e4b9bc747fa184d931255c9b4c55b35
habib-sc <habib97.cs@gmail.com>
2022-02-15 01:46:43 +0600

    items price increase decrease by quantity

--- js/app.js
+++ js/app.js
@@ -17,15 +17,23 @@ function cartQuantityUpdate(cartButtonClass, isPlus) {
             // cart quantitiy increasing or decreasing 
             if (isPlus) {
                 cartQuantityInput.value = cartQuantityValue + 1;
-            } else {
+            } else if (cartQuantityValue > 1) {
                 cartQuantityInput.value = cartQuantityValue - 1;
             }
 
             // price increase or decrease 
-
+            const itemPrice = cartButton.parentElement.nextElementSibling.lastChild;
+            const itemId = itemPrice.id;
+            const itemPriceInt = parseInt(itemPrice.innerText);
+            if (itemId == 'phone-price') {
+                itemPrice.innerText = parseInt(cartQuantityInput.value) * 1219;
+            } else if (itemId == 'case-price') {
+                itemPrice.innerText = parseInt(cartQuantityInput.value) * 59;
+            }
         })
     }
 }
+
 
 cartQuantityUpdate('cart-plus', true);
 cartQuantityUpdate('cart-minus', false);
