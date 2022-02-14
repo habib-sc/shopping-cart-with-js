b89f76f4411fd7413ac051776ee9610104b05efc
habib-sc <habib97.cs@gmail.com>
2022-02-15 00:37:41 +0600

    item quantity increase decrease by classname

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
-const cartPlusButtons = document.getElementsByClassName('cart-plus');
-for (const cartPlusButton of cartPlusButtons) {
-    cartPlusButton.addEventListener('click', function(event) {
-        const cartQuantityInput = event.target.parentNode.parentNode.childNodes[3];
-        let cartQuantityValue = parseInt(cartQuantityInput.value);
-        cartQuantityInput.value = cartQuantityValue + 1;
-        console.log(cartQuantityValue);
-    })
+function cartQuantityUpdate(cartButtonClass, isPlus) {
+    // Getting cart buttons 
+    const cartButtons = document.getElementsByClassName(cartButtonClass);
+    // Looping all the buttons 
+    for (const cartButton of cartButtons) {
+        // adding evnet listener spesific button 
+        cartButton.addEventListener('click', function(event) {
+            let cartQuantityInput = '';
+            // getting cart input by clicking button
+            if (isPlus) {
+                cartQuantityInput = cartButton.previousElementSibling;
+            } else {
+                cartQuantityInput = cartButton.nextElementSibling;
+            }
+            let cartQuantityValue = parseInt(cartQuantityInput.value);
+
+            // cart quantitiy increasing or decreasing 
+            if (isPlus) {
+                cartQuantityInput.value = cartQuantityValue + 1;
+            } else {
+                cartQuantityInput.value = cartQuantityValue - 1;
+            }
+
+            // price increase or decrease 
+
+        })
+    }
 }
+
+cartQuantityUpdate('cart-plus', true);
+cartQuantityUpdate('cart-minus', false);
